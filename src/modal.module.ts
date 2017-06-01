7058f43be7dcfecd3cce071d1f0a82a6e44a2aee
kageoni <sergiu@md.md>
2017-06-01 13:53:46 +0100

    ue trustHtml module instead of pipe increase the version to 0.1.7

--- src/modal.module.ts
+++ src/modal.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RxPubSub } from "rx-pubsub";
-import { TrustHtmlPipe } from "trust-html-pipe";
+import { TrustHtmlModule } from "trust-html";
 import { ComponentInjector } from "component-injector";
 import { Ng2ModalWindowComponent } from './components/ng2-modal-window/ng2-modal-window.component';
 import { Ng2ModalWindowService } from "./services/ng2-modal-window.service";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TrustHtmlModule
   ],
   declarations: [
-    Ng2ModalWindowComponent,
-    TrustHtmlPipe
+    Ng2ModalWindowComponent
   ],
   providers: [RxPubSub, Ng2ModalWindowService, ComponentInjector],
   exports: [Ng2ModalWindowComponent]
